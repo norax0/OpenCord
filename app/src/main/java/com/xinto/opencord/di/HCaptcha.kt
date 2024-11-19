97c47570eec9450b3ace67aecccb916637964094
norax0 <125187871+norax0@users.noreply.github.com>
2024-11-19 16:44:36 +0100

    fixed captcha

--- app/src/main/java/com/xinto/opencord/di/HCaptcha.kt
+++ app/src/main/java/com/xinto/opencord/di/HCaptcha.kt
@@ -4,24 +4,20 @@ import android.content.Context
 import com.hcaptcha.sdk.HCaptcha
 import com.hcaptcha.sdk.HCaptchaConfig
 import com.xinto.opencord.BuildConfig
-import org.koin.core.parameter.ParametersHolder
 import org.koin.dsl.module
 
 val hcaptchaModule = module {
     single { context ->
-        HCaptchaConfig.builder().apply {
-            siteKey(BuildConfig.CAPTCHA_KEY)
-            resetOnTimeout(true)
-            tokenExpiration(TOKEN_EXPIRATION)
-        }.build().let { config ->
-            HCaptcha.getClient(context as Context).apply { 
-                setup(config)
-            }
-        }
+        val config = HCaptchaConfig.builder()
+            .siteKey(BuildConfig.CAPTCHA_KEY)
+            .resetOnTimeout(true)
+            .tokenExpiration(TOKEN_EXPIRATION)
+            .build()
+        
+        HCaptcha.getClient(context as Context).setup(config)
     }
 
     companion object {
-        // Configurable constant for token expiration
-        private const val TOKEN_EXPIRATION = 5 * 60 * 1000L // 5 minutes
+        private const val TOKEN_EXPIRATION = 1 * 60 * 1000L // 1 minute
     }
 }
